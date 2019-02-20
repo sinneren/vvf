829d7c4e565bc968c7e7ca757da3a9344c349427
Sergey Laskin <s.laskin@site-mechanics.com>
2019-02-20 14:09:31 +0500

    refactor

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Books from '@/components/Books';
-import SignIn from '@/components/SignIn';
-import SignUp from '@/components/SignUp';
-import Profile from '@/components/Profile';
-import Logout from '@/components/Logout';
+import Books from './views/Books.vue';
+import SignIn from './views/SignIn.vue';
+import SignUp from './views/SignUp.vue';
+import Profile from './views/Profile.vue';
+import Logout from './views/Logout.vue';
 
 Vue.use(Router)
 
